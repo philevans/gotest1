b9e9b26c53d3ecf3655e57574adbc1c46a2ca1d9
Phil Evans <phil.evans@gmail.com>
2017-12-07 17:16:43 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println(math.Pi)
+	fmt.Println(math.SqrtPi)
+	var a chan int        	// Define a channel of type int
+	a = make(chan int, 1) 	// Actually make the channel with a buffer size of 1 item
+	a <- 4                	// Put an integer into the channel
+	fmt.Println(<-a)      	// Retrieve the integer from the channel and print it on screen
+}
